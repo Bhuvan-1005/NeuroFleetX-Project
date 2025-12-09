a4339bc816ac3938ae55e5925cd69ec4ec6a5c9e
Bhuvan-1005 <bhuvaneshwar2006@gmail.com>
2025-12-09 14:18:31 +0530

    Exercises Update

---
+++ Java_Programs/04_Loops/ForLoops.java
@@ -0,0 +1,13 @@
+﻿public class ForLoops {
+    public static void main(String[] args) {
+        System.out.println("Counting 1 to 5:");
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(i);
+        }
+
+        System.out.println("Multiplication Table of 2:");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("2 x " + i + " = " + (2 * i));
+        }
+    }
+}
